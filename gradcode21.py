4c445b41358c4b211980b59292572c0343399048
Ryan Dunning <ryandunning57@gmail.com>
2021-10-15 09:11:29 -0700

    made changes to code

--- gradcode21.py
+++ gradcode21.py
@@ -1,27 +1,24 @@
 import pandas as pd
 import numpy as np
+from keras.initializers.initializers_v2 import RandomNormal
 from numpy import mean
 from numpy import std
 import keras as ks
 import sklearn as skl
 import tensorflow as tf
-from sklearn.linear_model import  LinearRegression as LR
-from sklearn.metrics import accuracy_score
-from sklearn.model_selection import train_test_split, RepeatedStratifiedKFold, cross_val_score, KFold
-from sklearn.linear_model import  LogisticRegression as LogR
+from sklearn.metrics import accuracy_score, make_scorer
+from sklearn.model_selection import train_test_split, cross_val_score, KFold, RepeatedKFold
 from sklearn.preprocessing import LabelBinarizer, OneHotEncoder
 from keras.models import Sequential
 from keras.wrappers.scikit_learn import KerasRegressor
-from keras.metrics import RootMeanSquaredError
-from keras.metrics import MeanSquaredError
-from keras.layers import Dense
+from keras.backend import sqrt, mean, square
+from keras.layers import Dense, Dropout
 
 
 pd.set_option('display.max_columns', 50)
 pd.set_option('display.max_rows', 500)
-
+# tf.enable_eager_execution()
 pd.set_option('display.width', 750)
-
 le = skl.preprocessing.LabelEncoder()
 
 df = pd.read_csv('weather_and_fire.csv')
@@ -42,25 +39,38 @@ def root_mean_squared_error(y_true, y_pred):
     return np.sqrt(np.mean(np.square(y_true - y_pred)))
 
 def root_mean_squared_percent_error(y_true, y_pred):
-    return (np.sqrt(np.mean(np.square((y_true - y_pred)/y_true)))) * 100
+     rmspe = (sqrt(mean(square((y_true - y_pred) / y_true)))) * 100
+     return rmspe
+
+    #   rmspe = (ks.backend.sqrt(ks.backend.mean(ks.backend.square((y_true - y_pred) / y_true)))) * 100
+    # print(type(rmspe))
+    # thing = tf.compat.v1.placeholder(tf.float32)
+    # with tf.compat.v1.Session() as sess:
+    #     sess.run(rmspe.ref(), feed_dict={thing: y_pred})
+    # return thing
 
 def ann():
     model = Sequential()
-    model.add(Dense(64, input_dim=14,kernel_initializer="normal", activation='relu'))
+    model.add(Dense(64, input_dim=14,kernel_initializer=RandomNormal, activation='relu'))
+    model.add(Dropout(0.2))
     model.add(Dense(32, kernel_initializer='normal', activation='tanh'))
-    model.add(Dense(16, kernel_initializer='normal', activation='tanh'))
-    model.compile(loss=root_mean_squared_percent_error, optimizer='adam', metrics=['mse','mae',''])
+    model.add(Dropout(0.2))
+    model.add(Dense(32, kernel_initializer='normal', activation='tanh'))
+    model.add(Dropout(0.2))
+    model.add(Dense(1, kernel_initializer='normal', activation='tanh'))
+    model.compile(loss=root_mean_squared_percent_error, optimizer='adam', metrics=['mse',tf.keras.metrics.MeanAbsolutePercentageError(),tf.keras.metrics.MeanAbsoluteError()])
     return model
 
 X_train, X_test, Y_train, Y_test = train_test_split(X,y, test_size=0.33)
 
-estimator = KerasRegressor(build_fn=ann, epochs=25, batch_size=1, verbose=1)
-kfold = KFold(n_splits = 2)
-results = cross_val_score(estimator, X_test,Y_test, cv=kfold )
-print("Baseline: %.2f (%.2f) MSE" % (results.mean(), results.std()))
-estimator.fit(X_train,Y_train)
-prediction = estimator.predict(X_test)
-print(accuracy_score(Y_test, prediction))
 
+# rmspeScore = make_scorer(root_mean_squared_percent_error, greater_is_better=False)
+model = KerasRegressor(build_fn=ann, epochs=100, batch_size=32, verbose=1 )
+kfold = KFold(n_splits = 5)
+Y_train = Y_train.values.reshape(Y_train.size,1)
+results = cross_val_score(model, X_train,Y_train, cv=kfold) #scoring=rmspeScore)
+print("Wider: %.2f RMSPE (%.2f) Std deviation " % (results.mean(), results.std()))
 
-
+# model.fit(X_train,Y_train)
+# a = model.predict(X_test)
+# print("RMSE for Deep Network:",np.sqrt(np.mean((Y_test-a.reshape(a.size,))**2)))
